fb56d4c085eb0007dae25ef2cda5cfd74d86e4e5
Kacper Dabrowski <dabrowskik@int.pl>
2023-03-05 21:58:31 +0100

    - Naming improvements - Unnecessary variables removal - Overall code improvements

--- src/app.js
+++ src/app.js
@@ -3,101 +3,93 @@ const clearBtn = document.querySelector('.clear-btn');
 
 // to render table content from localStorage
 const tableContainer = document.querySelector('.table__body');
-const inputDate = document.querySelector('.form__input--date');
-const inputDescription = document.querySelector('.form__input--description');
-const inputTime = document.querySelector('.form__input--time');
+const workDateInput = document.querySelector('.form__input--date');
+const workDescriptionInput = document.querySelector('.form__input--description');
+const workDurationInput = document.querySelector('.form__input--time');
 //select options dropdown
-const select = document.querySelector('#options');
+const workTypeInput = document.querySelector('#workTypeInput');
+
+let spentTimes = [];
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-const valid = () => {
-  const dateValid = inputDate.value.trim();
-  const descriptionValid = inputDescription.value.trim();
-  const timeoutValid = inputTime.value.trim();
-  const selectValid = select.value;
-
-  if (timeoutValid === '') {
-    inputTime.classList.add('danger');
+const isWorkDateValid = () => {
+  const workDurationInputValue = workDurationInput.value;
+  if (!workDurationInputValue) {
+    workDurationInput.classList.add('danger');
     return false;
   }
 
   return true;
 };
-const calculateTime = (infos) => {
-  if (infos.length === 0) {
+
+const calculateTime = (spentTimes) => {
+  if (spentTimes.length === 0) {
     return 'Brak wykonanych zadań 😱';
   }
-  const numbers = infos.map((info) => Number(info.timeout));
+  const numbers = spentTimes.map((info) => Number(info.duration));
   const sum = numbers.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
   );
-  return `Całkowity czas wykonywanych zadań to: ${sum.toString()} min! 🫡`;
+  return `Całkowity czas wykonywanych zadań to: ${sum} min! 🫡`;
 };
 
 //clear input fields
 const clearFields = () => {
-  inputDate.value = '';
-  inputDescription.value = '';
-  inputTime.value = '';
-  select.value = '';
+  workDateInput.valueAsDate = new Date(); //set default value of workDateInput input in form to today
+  workDescriptionInput.value = null;
+  workDurationInput.value = null;
+  workTypeInput.value = null;
 };
 
 // render data to table from localStorage
 const renderData = () => {
-  let infos;
-  if (localStorage.getItem('array') === null) {
-    infos = [];
-  } else {
-    infos = JSON.parse(localStorage.getItem('array'));
-  }
-
-  const sumOfTime = calculateTime(infos);
-
-  if (infos.length === 0) {
+  const loadedSpentTimes = localStorage.getItem('spentTimes');
+  if (!loadedSpentTimes) {
+    spentTimes = [];
     clearBtn.classList.add('hidden');
   } else {
+    spentTimes = JSON.parse(loadedSpentTimes);
     clearBtn.classList.remove('hidden');
   }
 
-  const markup = `
-    ${infos
-      .map((info) => {
-        return `<tr>
-        <td class="table__body-data">${info.date}</td>
-        <td class="table__body-data">${info.timeout} min</td>
-        <td class="table__body-data">${info.description}</td>
-        <td class="table__body-data">${info.select.toUpperCase()}</td>
-        </tr>`;
-      })
-      .join('')}`;
+  const sumOfTime = calculateTime(spentTimes);
 
   const allTimeContainer = document.querySelector('.allTime');
   tableContainer.innerHTML = '';
   allTimeContainer.innerHTML = '';
   allTimeContainer.insertAdjacentHTML('beforeend', sumOfTime);
-  tableContainer.insertAdjacentHTML('beforeend', markup);
+  
+  workDateInput.valueAsDate = new Date(); //set default value of workDateInput input in form to today
+
+  if (spentTimes.length > 0) {
+    //there are values to show so render markup
+    const markup = `
+    ${spentTimes
+      .map((spentTime) => {
+        return `<tr>
+        <td class='table__body-data'>${spentTime.date}</td>
+        <td class='table__body-data'>${spentTime.duration} min</td>
+        <td class='table__body-data'>${spentTime.description}</td>
+        <td class='table__body-data'>${spentTime.type.toUpperCase()}</td>
+        </tr>`;
+      })
+      .join('')}`;
+    tableContainer.insertAdjacentHTML('beforeend', markup);
+  }
 };
 
 formContainer.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (valid()) {
-    console.log('frum drum brum');
-    const data = {
-      date: inputDate.value,
-      description: inputDescription.value,
-      timeout: inputTime.value,
-      select: select.value,
+  if (isWorkDateValid()) {
+    const work = {
+      date: workDateInput.value,
+      description: workDescriptionInput.value,
+      duration: workDurationInput.value,
+      type: workTypeInput.value,
     };
-
-    let infos;
-    if (localStorage.getItem('array') === null) {
-      infos = [];
-    } else {
-      infos = JSON.parse(localStorage.getItem('array'));
-    }
-    infos.push(data);
-    localStorage.setItem('array', JSON.stringify(infos));
+    spentTimes.push(work);
+    localStorage.setItem('spentTimes', JSON.stringify(spentTimes));
     renderData();
     clearFields();
   }
